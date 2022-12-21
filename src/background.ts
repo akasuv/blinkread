9a7eaf386aade80127d8b552435713a4f4bc2288
Wei Su <wei@funxociety.com>
2022-12-21 22:08:31 +0800

    update

--- src/background.ts
+++ src/background.ts
@@ -1,21 +1,24 @@
 // @ts-ignore
 import mount from "./content?script";
+
+const API_ENDPOINT = "https://blinkread-production.up.railway.app/summary";
+
 chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id as number },
-    files: [mount],
-  });
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id as number },
+		files: [mount],
+	});
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  fetch("http://localhost:3000/summary", {
-    method: "POST",
-    body: request.content,
-  })
-    .then((res) => {
-      return res.text();
-    })
-    .then((res) => sendResponse({ text: res }));
+	fetch(API_ENDPOINT, {
+		method: "POST",
+		body: request.content,
+	})
+		.then((res) => {
+			return res.text();
+		})
+		.then((res) => sendResponse({ text: res }));
 
-  return true;
+	return true;
 });
