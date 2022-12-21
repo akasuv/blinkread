9a5475c838ca47763e79f7edbef5b52952dff9a0
Wei Su <wei@funxociety.com>
2022-12-21 18:26:54 +0800

    update

--- src/content.tsx
+++ src/content.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import style from "./index.css?inline";
 
 const root = document.createElement("div");
 root.id = "blinkread-root";
 root.className =
-	"bg-white fixed top-8 right-8 z-[999] rounded-xl drop-shadow-lg";
+  "bg-white fixed top-8 right-8 z-[999] rounded-xl drop-shadow-lg";
+
+const styleEl = document.createElement("style");
+styleEl.textContent = style;
+
+document.body.append(styleEl);
 document.body.append(root);
 
 ReactDOM.createRoot(root).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
